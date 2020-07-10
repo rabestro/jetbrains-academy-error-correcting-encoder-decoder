99edc3f7728988fac751af6ef784d8be254784e4
Jegors Čemisovs <yegor.chemisov@gmail.com>
2020-07-10 23:43:47 +0300

    Solved stage 3

--- src/correcter/Application.java
+++ src/correcter/Application.java
@@ -1,6 +1,9 @@
 package correcter;
 
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Random;
@@ -16,11 +19,15 @@ public final class Application implements Runnable {
 
         try {
             final var message = Files.readString(Path.of("send.txt"));
-            final var brokenMessage = message
-                    .chars()
-                    .map(i -> i ^ (1 << random.nextInt(8)))
-                    .toArray();
+            final var buffer = ByteBuffer.allocate(message.length());
 
+            message.chars()
+                    .map(i -> i ^ (1 << random.nextInt(8)))
+                    .forEach(i -> buffer.put((byte) i));
+
+            FileChannel fc = new FileOutputStream("received.txt").getChannel();
+            fc.write(buffer.flip());
+            fc.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
