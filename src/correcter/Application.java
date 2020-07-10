37093098c4da0d77a219a8be14b486bf9b798487
Jegors Čemisovs <yegor.chemisov@gmail.com>
2020-07-10 23:30:24 +0300

    Deleted unused methods

--- src/correcter/Application.java
+++ src/correcter/Application.java
@@ -1,57 +1,29 @@
 package correcter;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.Random;
-import java.util.Scanner;
 import java.util.logging.Logger;
-import java.util.regex.Pattern;
-
-import static java.util.stream.Collectors.joining;
 
 public final class Application implements Runnable {
     private static final Logger log = Logger.getLogger(Application.class.getName());
     private static final Random random = new Random();
-    private static final Pattern SPLIT_BY_THREE = Pattern.compile("(?<=\\G...)");
-    private static final String ERRORS = "ABCDEFGHIJKLMNOPQRSTUVXYZ abcdefghijklmnopqrstuvxyz";
 
     @Override
     public void run() {
         log.fine("Application started");
-        final var scanner = new Scanner(System.in);
 
-        final var message = scanner.nextLine();
-        final var encoded = encode(message);
-        final var received = send(encoded);
-        final var decoded = decode(received);
+        try {
+            final var message = Files.readString(Path.of("send.txt"));
+            final var brokenMessage = message
+                    .chars()
+                    .map(i -> i ^ (1 << random.nextInt(8)))
+                    .toArray();
 
-        System.out.println(message);
-        System.out.println(encoded);
-        System.out.println(received);
-        System.out.println(decoded);
-        scanner.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
-    private String encode(final String message) {
-        return message.replaceAll("(.)", "$1$1$1");
-    }
-
-    private String send(final String message) {
-        return SPLIT_BY_THREE.splitAsStream(message).map(this::injectError).collect(joining());
-    }
-
-    private String decode(final String message) {
-        return SPLIT_BY_THREE.splitAsStream(message).map(this::fixError).collect(joining());
-    }
-
-    private String injectError(final String subString) {
-        final var chars = subString.toCharArray();
-        chars[random.nextInt(chars.length)] = ERRORS.charAt(random.nextInt(ERRORS.length()));
-        return new String(chars);
-    }
-
-    private String fixError(final String subString) {
-        final var a = subString.charAt(0);
-        final var b = subString.charAt(1);
-        final var c = subString.charAt(2);
-        return String.valueOf(a == b || a == c ? a : b);
-    }
 }
